ecf1ee600295ba56f1d529a6cc97bc4b638050bf
23hr03 <landaetahectorrenier@gmail.com>
2025-12-26 01:27:15 -0300

    fix: cors render exact frontend origin

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,11 @@ const app = express();
 ====================== */
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://utnfinal-frontend.onrender.com"
-    ],
+    origin: process.env.FRONTEND_ORIGIN,
     credentials: true,
   })
 );
+
 
 
 
